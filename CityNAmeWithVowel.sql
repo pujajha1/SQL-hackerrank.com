7e2eee9450c21f6fda0056375f8d4224313f7e23
Puja Jha <pujajha5912@gmail.com>
2019-07-08 14:12:10 +0530

    Create CityNAmeWithVowel.sql

---
+++ CityNAmeWithVowel.sql
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+
+
+SELECT DISTINCT CITY FROM STATION WHERE upper(substr(CITY,1,1)) in ('A','E','I','O','U') ORDER BY CITY;
