d17cd3a77526853ec0109737394c580395db597d
Puja Jha <pujajha5912@gmail.com>
2019-07-11 11:37:06 +0530

    Update CityEndingWithVowel.sql

--- BasicSelect/CityEndingWithVowel.sql
+++ BasicSelect/CityEndingWithVowel.sql
@@ -1,3 +1,5 @@
 #https://www.hackerrank.com/challenges/weather-observation-station-7/problem
 
-SELECT DISTINCT CITY FROM STATION WHERE UPPER(SUBSTR(CITY,-1)) IN ('A','E','I','O','U');
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE UPPER(SUBSTR(CITY,-1)) IN ('A','E','I','O','U');
